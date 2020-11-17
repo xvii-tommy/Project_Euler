81475dbf21168a6322009c9ddbfc23d7ab1b3f51
xvii-tommy <thomas.easterbrook97@gmail.com>
2020-11-17 18:06:12 +0000

    TE Problem_3.py

--- Problem_3.py
+++ Problem_3.py
@@ -26,14 +26,24 @@ class LPF:
         return(self.lst)
 
     def isprime(self, val):
-        for i in range(2, val):
-            if i**2 < val and val % i == 0:
-                print(val, "not prime")
+        for p in range(2, val):
+            if p**2 < val and val % p == 0:
+                self.templst.append(val)
+            elif p**2 > val:
+                break
+        return(self.templst)
 
     def largestprimefactor(self):
         f = self.factors()
         for i in f:
+            x = 0
             self.isprime(i)
+        for j in self.templst:
+            for k in f:
+                if j == k:
+                    f.remove(k)
+        print(f)
+        print("the largest prime is ", f[-1])
 
 
 lpf = LPF(600851475143)
