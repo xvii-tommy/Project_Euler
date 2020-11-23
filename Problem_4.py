e534d44e359110ca77a19f827bf45a6b0203190a
xvii-tommy <thomas.easterbrook97@gmail.com>
2020-11-23 18:43:18 +0000

    TE Problem_4

--- Problem_4.py
+++ Problem_4.py
@@ -3,35 +3,43 @@
 #Find the largest palindrome made from the product of two 3-digit numbers.
 # answer:
 
-class PAL:
+class PALINDROM:
     def __init__(self, val1, val2):
         self.val1 = val1
         self.val2 = val2
         self.lst = []
+        self.pal_lst = []
+        self.answer = []
 
-    def palindrom(self):
-        for x in range(self.val1, 1, -1):
-            for y in range(self.val2, 1, -1):
+    def find_values(self):
+        for x in range(self.val1, 100, -1):
+            for y in range(self.val2, 100, -1):
                 num = x * y
                 value = str(num)
-                if value[0] == value[-1]:
-                    self.lst.append(value)
-        self.ispal()
+                if value[0] == value[-1] and value[1] == value[-2]:
+                    if value not in self.lst:
+                        self.lst.append(value)
+        return self.lst
+
+    def reversepal(self, pal):
+        for i in pal:
+            i = i[::-1]
+            self.pal_lst.append(i)
+        return self.pal_lst
+
+    def find_palindroms(self):
+        x = self.find_values()
+        y = self.reversepal(x)
+        for i in range(len(x)):
+            if x[i] == y[i]:
+                self.answer.append(x[i])
+        for j in self.answer:
+            a = [int(i) for i in self.answer]
+        a.sort()
+        print(a)
+        print("The biggest palindrom is", a[-1])
 
 
-    def ispal(self):
-        for i in self.lst:
-            x = str(i)
-            fhalf = x[:round(len(x) / 2)]
-            shalf = reversed(x[round(len(x) / 2):])
-            for j, k in fhalf, shalf:
-                print(x, j,k)
-
-
-
-
-
-
-p = PAL(99, 99)
-p.palindrom()
+p = PALINDROM(999, 999)
+p.find_palindroms()
 
